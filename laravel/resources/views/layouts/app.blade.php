7282f5d6d2de5f9d34b25002dbd0f81a137248ae
Hasbi1605 <terry.delvon0805@gmail.com>
2026-04-15 15:19:13 +0700

    cleaning and update

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'ISTA AI') }}</title>
+        <title>ISTA AI</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/ista/logo.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
--- laravel/resources/views/profile.blade.php
+++ laravel/resources/views/profile.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'ISTA AI') }} - Profil</title>
+        <title>ISTA AI</title>
+
+        <link rel="icon" type="image/png" href="{{ asset('images/ista/logo.png') }}">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
